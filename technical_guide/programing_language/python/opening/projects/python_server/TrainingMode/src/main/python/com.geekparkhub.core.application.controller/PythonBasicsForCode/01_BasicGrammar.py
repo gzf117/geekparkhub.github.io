32d4906e1cbd7f4f53fbd4a047e100c088b45d51
GeekDeveloper <jeep-711@outlook.com>
2019-12-25 18:52:56 +0800

    update(technical_guide-programing_language: python_blog & python_projects)

---
+++ technical_guide/programing_language/python/opening/projects/python_server/TrainingMode/src/main/python/com.geekparkhub.core.application.controller/PythonBasicsForCode/01_BasicGrammar.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+#
+# Geek International Park | 极客国际公园
+# GeekParkHub | 极客实验室
+# Website | https://www.geekparkhub.com
+# Description | Open · Creation
+# Open Source Open Achievement Dream, GeekParkHub Co-construction has never been seen before.
+# HackerParkHub | 黑客公园
+# Website | https://www.hackerparkhub.org
+# Description | In the spirit of fearless exploration, create unknown technology and worship of technology.
+# GeekDeveloper : JEEP-711
+# 
+# @Author : system
+# @Version : 0.2.5
+# @Program : 基本语法
+# @File : 01_BasicGrammar.py
+# @Description : Python 基础篇 - 基本语法
+
+# 01 即表示注释
+
+# 02 print 打印语句 | Hello, World!
+print('Hello, World!')
+
+# 03 当前`\`表示换行
+print('当前\
+表示换行')
