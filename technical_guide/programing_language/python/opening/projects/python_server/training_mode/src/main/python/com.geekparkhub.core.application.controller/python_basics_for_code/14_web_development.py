ecfd8a1a0fc5af88212cdfe4ef0b22dd15c043c8
GeekDeveloper <jeep-711@outlook.com>
2020-01-24 16:11:27 +0800

    update(technical_guide-programing_language: python_blog & python_projects)

---
+++ technical_guide/programing_language/python/opening/projects/python_server/training_mode/src/main/python/com.geekparkhub.core.application.controller/python_basics_for_code/14_web_development.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# 
+# Geek International Park | 极客国际公园
+# GeekParkHub | 极客实验室
+# Website | https://www.geekparkhub.com
+# Description | Open · Creation | 
+# Open Source Open Achievement Dream, GeekParkHub Co-construction has never been seen before.
+# HackerParkHub | 黑客公园
+# Website | https://www.hackerparkhub.org
+# Description | In the spirit of fearless exploration, create unknown technology and worship of technology.
+# GeekDeveloper : JEEP-711
+# 
+# @Author : system
+# @Version : 0.2.5
+# @Program : Web开发 | Web development
+# @File : 14_web_development.py
+# @Description : Python 进阶篇 - Web开发 | Advanced Python - Web Development
